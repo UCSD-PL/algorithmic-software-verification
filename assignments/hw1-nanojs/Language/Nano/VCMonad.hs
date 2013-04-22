5c2b5aa3c6ae50602a4068a84ea094c67bea47f7
Valentin Robert <vrobert@cs.ucsd.edu>
2013-04-21 22:50:31 -0700

    moar nitpick

--- assignments/hw1-nanojs/Language/Nano/VCMonad.hs
+++ assignments/hw1-nanojs/Language/Nano/VCMonad.hs
@@ -2,14 +2,14 @@
 {- LANGUAGE FlexibleInstances    #-}
 {- LANGUAGE ScopedTypeVariables  #-}
 
--- | This module has the code for the VC-Generation Monad. 
+-- | This module has the code for the VC-Generation Monad.
 --   You /need not/ modify the code here, just use the exported API.
 
 module Language.Nano.VCMonad (
   -- * VC Monad
     VCM
- 
-  -- * Execute 
+
+  -- * Execute
   , execute
 
   -- * Side Conditions
@@ -21,32 +21,32 @@ module Language.Nano.VCMonad (
   , getFunctionPostcond
 
   -- * Lookup Specification for Callee
-  , getCalleeSpec 
+  , getCalleeSpec
 
   -- * Generate a fresh Id
-  , freshId 
+  , freshId
 
-  )  where 
+  )  where
 
 import qualified Data.HashMap.Strict as M
 import           Control.Applicative          ((<$>))
 import           Control.Monad.State
 import           Language.ECMAScript3.Syntax
 import qualified Language.Fixpoint.Types as F
-import           Language.Fixpoint.Misc 
+import           Language.Fixpoint.Misc
 import           Language.Nano.Types
 import           Data.Monoid
 import           Data.Maybe                   (fromMaybe)
 -- import           Text.Parsec                  (SourcePos)
 
 --------------------------------------------------------------------
--- | `VCM` is a VCGen monad that logs the loop-inv "side conditions" 
+-- | `VCM` is a VCGen monad that logs the loop-inv "side conditions"
 --------------------------------------------------------------------
 
-data VCState = VCS { vc_vc    :: !VCond 
-                   , vc_spec  :: M.HashMap String (Fun SourcePos)  
-                   , vc_count :: !Int 
-                   , vc_fun   :: Maybe (Fun SourcePos) 
+data VCState = VCS { vc_vc    :: !VCond
+                   , vc_spec  :: M.HashMap String (Fun SourcePos)
+                   , vc_count :: !Int
+                   , vc_fun   :: Maybe (Fun SourcePos)
                    }
 
 type VCM     = State VCState
@@ -56,8 +56,8 @@ type VCM     = State VCState
 -------------------------------------------------------------------
 execute :: Nano -> VCM a -> a
 -------------------------------------------------------------------
-execute pgm act   = fst $ runState act init 
-  where 
+execute pgm act   = fst $ runState act init
+  where
     init          = VCS mempty sp 0 Nothing
     sp            = M.fromList [(name $ fname f, f) | f <- pgm]
     name (Id _ x) = x
@@ -76,9 +76,9 @@ getSideCond = vc_vc <$> get
 
 
 -------------------------------------------------------------------
-setFunction :: Fun SourcePos -> VCM () 
+setFunction :: Fun SourcePos -> VCM ()
 -------------------------------------------------------------------
-setFunction fn = modify $ \st -> st { vc_fun = Just fn } 
+setFunction fn = modify $ \st -> st { vc_fun = Just fn }
                                     { vc_vc  = mempty }
 
 
@@ -86,21 +86,21 @@ setFunction fn = modify $ \st -> st { vc_fun = Just fn }
 getFunctionPostcond :: VCM F.Pred
 -------------------------------------------------------------------
 getFunctionPostcond = maybe err fpost . vc_fun <$> get
-  where 
+  where
     err             = errorstar "No Current Function: getPostcond"
 
 -------------------------------------------------------------------
 freshId      :: a -> VCM (Id a)
 -------------------------------------------------------------------
-freshId l    = do st    <- get 
-                  put    $ st { vc_count = 1 + (vc_count st) } 
+freshId l    = do st    <- get
+                  put    $ st { vc_count = 1 + (vc_count st) }
                   return $ Id l $ "nanoTmp" ++ show (vc_count st)
 
 -------------------------------------------------------------------
 getCalleeSpec :: String -> VCM (Fun SourcePos)
 -------------------------------------------------------------------
 getCalleeSpec f = (fromMaybe err . M.lookup f . vc_spec) <$> get
-  where 
-    err         = errorstar $ "Unknown Function:" ++ f 
+  where
+    err         = errorstar $ "Unknown Function:" ++ f
 
 
